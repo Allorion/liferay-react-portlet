c0dd08136c7d6e6184810d57eef2ffadebe65624
Allori <wlad70657@gmail.com>
2022-01-21 15:32:05 +0300

    Update deploy css
    Добавлена возможность подгрузки стилей импортированных из .jsx

--- Default portlet/allorion-mvc-portlet-project/webpack.config.js
+++ Default portlet/allorion-mvc-portlet-project/webpack.config.js
@@ -26,23 +26,9 @@ module.exports = {
                 use: ['babel-loader'],
                 exclude: path.resolve(__dirname, "node_modules")
             },
-            {test: /\.css$/, use: ['style-loader!css-loader']},
-            {test: /\.(s[ca]ss)$/,
-                use: ['style-loader', 'css-loader', 'sass-loader', {
-                    loader: "postcss-loader",
-                    options: {
-                        postcssOptions: {
-                            plugins: [
-                                [
-                                    "postcss-preset-env",
-                                    {
-                                        // Options
-                                    },
-                                ],
-                            ],
-                        },
-                    },
-                }]
+            {
+                test: /\.(css|scss)$/,
+                loader: "style-loader!css-loader"
             },
             {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, use: ['url?limit=10000&amp;mimetype=application/font-woff']},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, use: ['url?limit=10000&amp;mimetype=application/octet-stream']},
